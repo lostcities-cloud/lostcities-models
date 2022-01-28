524f7b9a9df30d9a6d73e553d7930bf1d08b5b93
Derek <contact@dereknelson.io>
2022-01-28 15:52:43 -0500

    Fixing gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,41 @@
+import org.gradle.api.tasks.bundling.Jar
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.6.10"
 }
 
-group = "o.dereknelson.lostcities"
+group = "io.dereknelson.lostcities"
 version = "1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
 repositories {
-    maven {
-        url = uri("https://maven.pkg.github.com/lostcities-cloud/lostcities-common")
-        credentials {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-        }
-    }
     mavenCentral()
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+
+    repositories {
+
+        publications {
+            register("mavenJava", MavenPublication::class) {
+                from(components["java"])
+                artifact(sourcesJar.get())
+            }
+        }
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/lostcities-cloud/lostcities-models")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
