0153d998950b4e720634e5712e928b5f18fd5033
Derek <contact@dereknelson.io>
2022-02-23 22:18:43 -0500

    Removing jsonignore from cardnumber in Card.kt

---
+++ src/main/kotlin/io/dereknelson/lostcities/models/matches/FinishMatchEvent.kt
@@ -0,0 +1,9 @@
+package io.dereknelson.lostcities.models.matches
+
+import java.time.LocalDateTime
+
+data class FinishMatchEvent(
+    val id: Long,
+    val scores: Map<String, Int>,
+    val finishedAt: LocalDateTime
+)
