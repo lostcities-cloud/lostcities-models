996c72702c3457442cdcd581211d69e1ebed6133
nelson54 <me@dereknelson.io>
2024-11-04 17:09:13 -0500

    Fix logging for matches

--- src/main/kotlin/io/dereknelson/lostcities/models/matches/TurnChangeEvent.kt
+++ src/main/kotlin/io/dereknelson/lostcities/models/matches/TurnChangeEvent.kt
@@ -3,4 +3,9 @@ package io.dereknelson.lostcities.models.matches
 class TurnChangeEvent(
     val matchId: Long,
     val nextPlayer: String,
-)
+
+) {
+    override fun toString(): String {
+        return "TurnChangeEvent(matchId=$matchId, nextPlayer='$nextPlayer')"
+    }
+}
