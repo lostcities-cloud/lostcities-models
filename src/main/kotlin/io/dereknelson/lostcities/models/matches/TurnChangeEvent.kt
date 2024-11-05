a54d19e266493bb7d32ab514eb259eb9b35e1eef
nelson54 <me@dereknelson.io>
2024-11-04 19:17:45 -0500

    Adding turns to turn change.

--- src/main/kotlin/io/dereknelson/lostcities/models/matches/TurnChangeEvent.kt
+++ src/main/kotlin/io/dereknelson/lostcities/models/matches/TurnChangeEvent.kt
@@ -3,9 +3,9 @@ package io.dereknelson.lostcities.models.matches
 class TurnChangeEvent(
     val matchId: Long,
     val nextPlayer: String,
-
+    val turns: Long,
 ) {
     override fun toString(): String {
-        return "TurnChangeEvent(matchId=$matchId, nextPlayer='$nextPlayer')"
+        return "TurnChangeEvent(matchId=$matchId, nextPlayer='$nextPlayer', turns=$turns)"
     }
 }
